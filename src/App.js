afbe52e358e9bf0cc692b0e7c09ded3dabb6935f
sanjana awachat <you@example.com>
2022-08-14 15:25:56 +0530

    assessment2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
-
+import Employee_feedback_form from './Js/Employee_feedback_form';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Employee_feedback_form/>
     </div>
   );
 }
